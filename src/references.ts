443a51229ed807d34ecb6590083f584ed2b30a8e
Joseph Schultz <joseph@acupajoe.io>
2018-09-04 16:14:20 -0500

    Updated references

--- src/references.ts
+++ src/references.ts
@@ -3,14 +3,6 @@ import { Reference } from "./types";
 // Records should be added here to be indexed / made searchable
 const references: Array<Reference> = [
   {
-    collection: 'chats',
-    subcollection: 'posts',
-    index: 'chat-posts',
-    type: 'chat-posts',
-    transform: (data, parentSnap) => ({...data, chatId: parentSnap.id}),
-    include: ['userId', 'userName', 'content', 'type', 'createdAt', 'updatedAt']
-  },
-  {
     collection: 'dioceses',
     type: 'dioceses',
     index: 'dioceses',
@@ -21,19 +13,16 @@ const references: Array<Reference> = [
     type: 'groups',
     index: 'groups',
     include: ['name', 'location', 'profile'],
+    mappings: {
+      geopoint: {
+        type: 'geo_point'
+      }
+    },
     transform: (doc) => ({
       ...doc.location,
       ...doc.profile,
-      geohash: `${doc.location.geopoint._latitude},${doc.location.geopoint._longitude}`
+      geopoint: `${doc.location.geopoint._latitude},${doc.location.geopoint._longitude}`
     }),
-    builder: (ref) => ref.limit(100)
-  },
-  {
-    collection: 'organizations',
-    type: 'organizations',
-    index: 'organizations',
-    include: ['name', 'location', 'profile'],
-    builder: (ref) => ref.limit(100)
   },
   {
     collection: 'prayers',
